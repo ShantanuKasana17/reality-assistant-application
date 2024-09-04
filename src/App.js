e11f062220b9345a603b36137411fae229e7991d
Akshit Yadav <akshit07032001@gmail.com>
2024-09-04 22:16:59 +0530

    feat: location tracking

--- src/App.js
+++ src/App.js
@@ -1,19 +1,62 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import { Header } from './container';
-import { Navbar } from './components';
-import './App.css';
-import SpecialMenu from './container/SpecialMenu/SpecialMenu';
+import { Header } from "./container";
+import { Navbar } from "./components";
+import "./App.css";
+import SpecialMenu from "./container/SpecialMenu/SpecialMenu";
 const App = () => {
   const [cartItems, setCartItems] = useState([]);
+  const [userLocation, setUserLocation] = useState(null);
+  const [isInValidLocation, setIsInValidLocation] = useState(false);
 
-  return(
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setUserLocation({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          });
+          checkValidLocation(
+            position.coords.latitude,
+            position.coords.longitude
+          );
+        },
+        (error) => {
+          console.error("Error getting location:", error);
+          setIsInValidLocation(true);
+        }
+      );
+    } else {
+      console.log("Geolocation is not available");
+      setIsInValidLocation(true);
+    }
+    console.log(isInValidLocation);
+  }, []);
+
+  const checkValidLocation = (latitude, longitude) => {
+    // Define your valid location range here
+    const validLatitude = [17.4257427, 17.4401427]; 
+    const validLongitude = [78.5111404, 78.5261404]; 
+
+    console.log(latitude, longitude);
+    setIsInValidLocation(
+      latitude < validLatitude[0] ||
+        latitude > validLatitude[1] ||
+        longitude < validLongitude[0] ||
+        longitude > validLongitude[1]
+    );
+  };
+
+  return (
     <>
       <Navbar cartItems={cartItems} setCartItems={setCartItems} />
-      <Header />
-      <SpecialMenu cartItems={cartItems} setCartItems={setCartItems} />
+      <Header isInValidLocation={isInValidLocation} />
+      {!isInValidLocation && (
+        <SpecialMenu cartItems={cartItems} setCartItems={setCartItems} />
+      )}
     </>
-  )
+  );
 };
 
 export default App;
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,26 +1,45 @@
-import React from 'react';
+import React from "react";
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
+import "./Header.css";
 
-import { SubHeading } from '../../components';
-import { images } from '../../constants';
-import './Header.css';
-
-const Header = () => {
+const Header = ({ isInValidLocation }) => {
   const params = new URLSearchParams(window.location.search);
-  const tableNumber = params.get('table');
+  const tableNumber = params.get("table");
 
-  return(
+  return (
     <div className="app__header app__wrapper section__padding vh-80" id="home">
       <div className="app__wrapper_info">
-        <SubHeading title={tableNumber==null?`Welcome Guest`:`Welcome Table No. ${tableNumber}`} />
-        <h1 className="app__header-h1">Chase The New Flavour</h1>
-        <p className="p__opensans" style={{ margin: '2rem 0' }}>Enjoy our complimentary snacks and beverages, thoughtfully curated for your refreshments</p>
-        <button type="button" className="custom__button"><a href="#menu">Explore Menu</a></button>
+        {isInValidLocation ? (
+          <p className="p__opensans" style={{ margin: "2rem 0" }}>
+            We're sorry, but our special menu is not available in your current
+            location.
+          </p>
+        ) : (
+          <>
+            <SubHeading
+              title={
+                tableNumber == null
+                  ? `Welcome Guest`
+                  : `Welcome Table No. ${tableNumber}`
+              }
+            />
+            <h1 className="app__header-h1">Chase The New Flavour</h1>
+            <p className="p__opensans" style={{ margin: "2rem 0" }}>
+              Enjoy our complimentary snacks and beverages, thoughtfully curated
+              for your refreshments.
+            </p>
+            <button type="button" className="custom__button">
+              <a href="#menu">Explore Menu</a>
+            </button>
+          </>
+        )}
       </div>
 
-    {/* <div className="app__wrapper_img">
-      <img src={images.welcome} alt="header_img" />
-    </div> */}
-  </div>
+      {/* <div className="app__wrapper_img">
+        <img src={images.welcome} alt="header_img" />
+      </div> */}
+    </div>
   );
 };
 
